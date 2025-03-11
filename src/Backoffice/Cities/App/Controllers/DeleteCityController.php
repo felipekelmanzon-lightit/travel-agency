699494e8cccf416b8a6dde04b5d60ad59f86a491
felipekelmanzon-lightit <felipe.kelmanzon@lightit.io>
2025-03-11 13:53:16 -0300

    fix: delete DeleteCityAction

--- src/Backoffice/Cities/App/Controllers/DeleteCityController.php
+++ src/Backoffice/Cities/App/Controllers/DeleteCityController.php
@@ -6,14 +6,13 @@ namespace Lightit\Backoffice\Cities\App\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Lightit\Backoffice\Cities\App\Transformers\CityTransformer;
-use Lightit\Backoffice\Cities\Domain\Actions\DeleteCityAction;
 use Lightit\Backoffice\Cities\Domain\Models\City;
 
 class DeleteCityController
 {
-    public function __invoke(DeleteCityAction $action, City $city): JsonResponse
+    public function __invoke(City $city): JsonResponse
     {
-        $action->execute($city);
+        $city->delete();
 
         return responder()
             ->success(CityTransformer::class)
